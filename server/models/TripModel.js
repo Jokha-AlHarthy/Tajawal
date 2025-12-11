e14604b8d30a2fd91369a2d5a15c810d739a43e2
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2025-12-12 01:09:47 +0400

    Update TripModel.js

--- server/models/TripModel.js
+++ server/models/TripModel.js
@@ -3,14 +3,20 @@ import mongoose from "mongoose";
 const TripSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "Users", required: true },
   destination: { type: String, required: true },
-  startDate: { type: String, required: true },  
-  endDate: { type: String, required: true },    
+  startDate: { type: String, required: true },
+  endDate: { type: String, required: true },
   budget: { type: Number, required: true },
   travelers: { type: Number, required: true },
   interests: { type: [String], required: true },
+  lat: { type: Number },
+  lng: { type: Number },
   createdAt: { type: Date, default: Date.now },
-  destination: { type: String, required: true }
-
+  activities: [
+  {
+    day: Number,
+    items: [String]
+  }
+],
 });
 
 export default mongoose.model("Trips", TripSchema);
