dd5d8e5aabd583d18a8def8b7b911b7266a28482
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2025-12-12 01:08:13 +0400

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import UserReducer from './features/UserSlice';
 import tripReducer from "./features/TripSlice";
 import adminReducer from "./features/AdminSlice";
+import notificationsReducer from "./features/NotificationSlice";
 
 export const store=configureStore({
     reducer:{
         users:UserReducer,
         trip: tripReducer,
         admin: adminReducer, 
+        notifications: notificationsReducer,
     }
 });
