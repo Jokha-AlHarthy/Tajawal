7ceab127b53383d344568bc18c8474bc9fe2d788
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2025-12-13 17:48:30 +0400

    Update AdminNavbar.js

--- client/src/components/AdminNavbar.js
+++ client/src/components/AdminNavbar.js
@@ -1,22 +1,11 @@
-// src/components/admin/AdminNavbar.jsx
-
 import React, { useState, useEffect } from "react";
 import { useLocation, Link } from "react-router-dom";
 import Logo from "../assests/logo.png";
-import {
-  FaGlobe,
-  FaUsersCog,
-  FaUserCircle,
-  FaSignOutAlt,
-  FaMoon,
-  FaSun
-} from "react-icons/fa";
+import {FaGlobe,FaUsersCog,FaUserCircle,FaSignOutAlt,FaMoon,FaSun} from "react-icons/fa";
 
 const AdminNavbar = ({ theme, toggleTheme }) => {
   const [activeItem, setActiveItem] = useState("");
   const location = useLocation();
-
-  // Map routes to menu items
   useEffect(() => {
     const map = {
       "/admin/destinations": "destinations",
@@ -26,8 +15,6 @@ const AdminNavbar = ({ theme, toggleTheme }) => {
     };
     setActiveItem(map[location.pathname] || "");
   }, [location.pathname]);
-
-  // Dynamic color based on active item + theme
   const getColor = (item) => {
     if (activeItem === item) {
       return theme === "light" ? "#F5A623" : "#DDE9F7";
@@ -48,10 +35,7 @@ const AdminNavbar = ({ theme, toggleTheme }) => {
         boxShadow: "0 2px 6px rgba(0,0,0,0.1)"
       }}
     >
-      {/* Logo */}
       <img src={Logo} alt="logo" style={{ width: "140px" }} />
-
-      {/* Right side menu */}
       <div style={{ display: "flex", alignItems: "center", gap: "35px" }}>
         <Link
           to="/admin/destinations"
@@ -63,7 +47,6 @@ const AdminNavbar = ({ theme, toggleTheme }) => {
         >
           <FaGlobe style={{ marginRight: 5 }} /> Destinations
         </Link>
-
         <Link
           to="/admin/users"
           style={{
@@ -74,7 +57,6 @@ const AdminNavbar = ({ theme, toggleTheme }) => {
         >
           <FaUsersCog style={{ marginRight: 5 }} /> Users
         </Link>
-
         <Link
           to="/admin/profile"
           style={{
@@ -85,7 +67,6 @@ const AdminNavbar = ({ theme, toggleTheme }) => {
         >
           <FaUserCircle style={{ marginRight: 5 }} /> Profile
         </Link>
-
         <Link
           to="/"
           style={{
@@ -96,8 +77,6 @@ const AdminNavbar = ({ theme, toggleTheme }) => {
         >
           <FaSignOutAlt style={{ marginRight: 5 }} /> Logout
         </Link>
-
-        {/* 🔆 Theme toggle – icon is inside the clickable div */}
         <div
           onClick={toggleTheme}
           style={{
