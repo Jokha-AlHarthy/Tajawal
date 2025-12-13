a43ae083df6584108dcceb95d888479ee9e25de5
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2025-12-13 17:55:18 +0400

    Create NotificationSlice.js

---
+++ client/src/features/NotificationSlice.js
@@ -0,0 +1,66 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+// Load user's notifications
+export const fetchNotifications = createAsyncThunk(
+  "notifications/fetch",
+  async (userId) => {
+    const res = await axios.get(`http://localhost:8080/notifications/user/${userId}`);
+    return res.data;
+  }
+);
+
+// Add notification to backend
+export const addNotificationToDB = createAsyncThunk(
+  "notifications/add",
+  async (data) => {
+    const res = await axios.post("http://localhost:8080/notifications/add", data);
+    return res.data;
+  }
+);
+
+// Mark notification as read
+export const markReadInDB = createAsyncThunk(
+  "notifications/markRead",
+  async (id) => {
+    const res = await axios.put(`http://localhost:8080/notifications/read/${id}`);
+    return res.data;
+  }
+);
+
+// Delete notification
+export const deleteNotificationFromDB = createAsyncThunk(
+  "notifications/delete",
+  async (id) => {
+    await axios.delete(`http://localhost:8080/notifications/delete/${id}`);
+    return id;
+  }
+);
+
+const notificationSlice = createSlice({
+  name: "notifications",
+  initialState: {
+    list: [],
+    loading: false,
+  },
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchNotifications.fulfilled, (state, action) => {
+        state.list = action.payload;
+      })
+      .addCase(addNotificationToDB.fulfilled, (state, action) => {
+        state.list.push(action.payload);
+      })
+      .addCase(markReadInDB.fulfilled, (state, action) => {
+        const index = state.list.findIndex(n => n._id === action.payload._id);
+        if (index !== -1) state.list[index] = action.payload;
+      })
+      .addCase(deleteNotificationFromDB.fulfilled, (state, action) => {
+        state.list = state.list.filter(n => n._id !== action.payload);
+      });
+  },
+});
+
+export default notificationSlice.reducer;
