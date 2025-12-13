b38df6e07a0f9e47b367edc4d057babd528092d2
Jokha Al-Harthy <jokha.hamed.official@gmail.com>
2025-12-13 17:54:49 +0400

    Update AdminSlice.js

--- client/src/features/AdminSlice.js
+++ client/src/features/AdminSlice.js
@@ -1,6 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+
+//Getting the destination form the database (Read)
 export const getAllDestinations = createAsyncThunk("admin/getAllDestinations", async () => {
     try {
         const res = await axios.get("http://localhost:8080/admin/destinations");
@@ -10,6 +12,7 @@ export const getAllDestinations = createAsyncThunk("admin/getAllDestinations", a
     }
 });
 
+//Deleteting the destination from the database (Delete)
 export const deleteDestination = createAsyncThunk("admin/deleteDestination", async (did) => {
     try {
         const res = await axios.delete(`http://localhost:8080/admin/destination/delete/${did}`);
@@ -19,6 +22,7 @@ export const deleteDestination = createAsyncThunk("admin/deleteDestination", asy
     }
 });
 
+//Adding the destination in the database (Add)
 export const addDestination = createAsyncThunk("admin/addDestination", async (data) => {
     try {
         const res = await axios.post("http://localhost:8080/admin/destination/add", data);
@@ -28,6 +32,8 @@ export const addDestination = createAsyncThunk("admin/addDestination", async (da
     }
 });
 
+
+//Getting the registred users from the database (Read)
 export const getAllUsers = createAsyncThunk("admin/getAllUsers", async () => {
     try {
         const res = await axios.get("http://localhost:8080/admin/users");
@@ -37,6 +43,8 @@ export const getAllUsers = createAsyncThunk("admin/getAllUsers", async () => {
     }
 });
 
+
+//Deleting the user from the website (Delete)
 export const deleteUser = createAsyncThunk("admin/deleteUser", async (email) => {
     try {
         const res = await axios.delete(`http://localhost:8080/admin/user/delete/${email}`);
@@ -46,15 +54,22 @@ export const deleteUser = createAsyncThunk("admin/deleteUser", async (email) => 
     }
 });
 
-export const updateAdminProfile = createAsyncThunk("admin/updateAdminProfile", async (pdata) => {
-    try {
-        const res = await axios.put("http://localhost:8080/admin/profile/update", pdata);
-        return res.data;
-    } catch (error) {
-        console.log(error);
-    }
-});
 
+//Update Admin profile (Update)
+export const updateAdminProfile = createAsyncThunk("admin/updateAdminProfile",async (formData) => {
+    const res = await axios.put(
+      "http://localhost:8080/admin/profile/update",
+      formData,
+      {
+        headers: { "Content-Type": "multipart/form-data" }
+      }
+    );
+    return res.data;
+  }
+);
+
+
+//Updating the user profile (Update)
 export const updateUser = createAsyncThunk("admin/updateUser", async (data) => {
     try {
         const res = await axios.put("http://localhost:8080/admin/user/update", data);
@@ -79,103 +94,111 @@ const initVal = {
 export const AdminSlice = createSlice({
     name: "admin",
     initialState: initVal,
-    reducers: {},
+    reducers: {
+        setAdmin: (state, action) => {
+            state.admin = action.payload;
+            localStorage.setItem("admin", JSON.stringify(action.payload));
+        }
+    },
     extraReducers: (builder) => {
         builder
-        .addCase(getAllDestinations.pending, (state) => {
-            state.isLoading = true;
-        })
-        .addCase(getAllDestinations.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.isSuccess = true;
-            state.destinations = action.payload || [];
-        })
-        .addCase(getAllDestinations.rejected, (state) => {
-            state.isLoading = false;
-            state.isError = true;
-        })
 
-        .addCase(addDestination.pending, (state) => {
-            state.isLoading = true;
-        })
-        .addCase(addDestination.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.isSuccess = true;
-            state.message = action.payload ? action.payload.message : "";
-        })
-        .addCase(addDestination.rejected, (state) => {
-            state.isLoading = false;
-            state.isError = true;
-        })
 
-        .addCase(deleteDestination.pending, (state) => {
-            state.isLoading = true;
-        })
-        .addCase(deleteDestination.fulfilled, (state) => {
-            state.isLoading = false;
-            state.isSuccess = true;
-        })
-        .addCase(deleteDestination.rejected, (state) => {
-            state.isLoading = false;
-            state.isError = true;
-        })
+            .addCase(getAllDestinations.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getAllDestinations.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.destinations = action.payload || [];
+            })
+            .addCase(getAllDestinations.rejected, (state) => {
+                state.isLoading = false;
+                state.isError = true;
+            })
 
-        .addCase(getAllUsers.pending, (state) => {
-            state.isLoading = true;
-        })
-        .addCase(getAllUsers.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.isSuccess = true;
-            state.users = action.payload || [];
-        })
-        .addCase(getAllUsers.rejected, (state) => {
-            state.isLoading = false;
-            state.isError = true;
-        })
+            .addCase(addDestination.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(addDestination.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.message = action.payload ? action.payload.message : "";
+            })
+            .addCase(addDestination.rejected, (state) => {
+                state.isLoading = false;
+                state.isError = true;
+            })
 
-        .addCase(deleteUser.pending, (state) => {
-            state.isLoading = true;
-        })
-        .addCase(deleteUser.fulfilled, (state) => {
-            state.isLoading = false;
-            state.isSuccess = true;
-        })
-        .addCase(deleteUser.rejected, (state) => {
-            state.isLoading = false;
-            state.isError = true;
-        })
+            .addCase(deleteDestination.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(deleteDestination.fulfilled, (state) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+            })
+            .addCase(deleteDestination.rejected, (state) => {
+                state.isLoading = false;
+                state.isError = true;
+            })
 
-        .addCase(updateAdminProfile.pending, (state) => {
-            state.isLoading = true;
-        })
-        .addCase(updateAdminProfile.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.isSuccess = true;
+            .addCase(getAllUsers.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getAllUsers.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.users = action.payload || [];
+            })
+            .addCase(getAllUsers.rejected, (state) => {
+                state.isLoading = false;
+                state.isError = true;
+            })
 
-            if (action.payload) {
-                state.admin = action.payload.admin;
+            .addCase(deleteUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(deleteUser.fulfilled, (state) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+            })
+            .addCase(deleteUser.rejected, (state) => {
+                state.isLoading = false;
+                state.isError = true;
+            })
+
+            .addCase(updateAdminProfile.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(updateAdminProfile.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+
+                if (action.payload) {
+                    state.admin = action.payload.admin;
+                    state.message = action.payload.message;
+                    localStorage.setItem("admin", JSON.stringify(action.payload.admin));
+                }
+            })
+            .addCase(updateAdminProfile.rejected, (state) => {
+                state.isLoading = false;
+                state.isError = true;
+            })
+
+            .addCase(updateUser.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(updateUser.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
                 state.message = action.payload.message;
-                localStorage.setItem("admin", JSON.stringify(action.payload.admin));
-            }
-        })
-        .addCase(updateAdminProfile.rejected, (state) => {
-            state.isLoading = false;
-            state.isError = true;
-        })
-
-        .addCase(updateUser.pending, (state) => {
-            state.isLoading = true;
-        })
-        .addCase(updateUser.fulfilled, (state, action) => {
-            state.isLoading = false;
-            state.isSuccess = true;
-            state.message = action.payload.message;
-        })
-        .addCase(updateUser.rejected, (state) => {
-            state.isLoading = false;
-            state.isError = true;
-        });
+            })
+            .addCase(updateUser.rejected, (state) => {
+                state.isLoading = false;
+                state.isError = true;
+            });
     }
 });
 
+export const { setAdmin } = AdminSlice.actions;
 export default AdminSlice.reducer;
